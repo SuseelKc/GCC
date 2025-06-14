57acf9b24fde78d3f676a4065f5fa9da4c04d9bc
Sushil Khatri <sushil.khatri@cibnepal.org.np>
2025-06-14 13:23:23 +0545

    chnages

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,26 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ContactController extends Controller
 {
+    /**
+     * Display the contact form.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
         return view('contact');
     }
 
+    /**
+     * Handle the contact form submission.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(Request $request)
     {
-        // TODO: Implement contact form submission
-        // Validate the request
-        $validated = $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|email|max:255',
+            'subject' => 'required|string|max:255',
             'message' => 'required|string',
+        ], [
+            'name.required' => 'Please enter your name.',
+            'email.required' => 'Please enter your email address.',
+            'email.email' => 'Please enter a valid email address.',
+            'subject.required' => 'Please enter a subject.',
+            'message.required' => 'Please enter your message.',
         ]);
 
-        // TODO: Send email or store in database
-        
-        return redirect()->route('contact')->with('success', 'Thank you for your message. We will get back to you soon!');
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+
+        // TODO: Add your contact form processing logic here
+        // For example, you might want to:
+        // 1. Store the message in a database
+        // 2. Send an email notification
+        // 3. Send a confirmation email to the user
+
+        return back()->with('contact-success', 'Thank you for your message. We will get back to you soon!');
     }
 } 
